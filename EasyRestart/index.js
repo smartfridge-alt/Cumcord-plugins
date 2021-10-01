d908ac39d66aa1d70c6b4fe1b77c7ab0857db341
smartfrigde <smartfrigde@users.noreply.github.com>
2021-10-01 21:23:22 +0000

    deploy: d15bcc863e392daf4127851b883ffefb039e4ec6

--- EasyRestart/index.js
+++ EasyRestart/index.js
@@ -1,29 +1,9 @@
-import { webpackModules } from "@cumcord/modules";
+
 let keydownHandler;
-function showConfirmationModal(title, content, options = {}) {
-    const ModalActions = webpackModules.findByProps('openModal', 'updateModal');
-    const Markdown = webpackModules.findByDisplayName('Markdown');
-    const ConfirmationModal = webpackModules.findByDisplayName('ConfirmModal');
-    if (!ModalActions || !ConfirmationModal || !Markdown) return window.alert(content);
 
-    const emptyFunction = () => {};
-    const {onConfirm = emptyFunction, onCancel = emptyFunction, confirmText = 'Okay', cancelText = 'Cancel', danger = false, key = undefined} = options;
 
-    if (!Array.isArray(content)) content = [content];
-    content = content.map(c => typeof(c) === 'string' ? webpackModules.findByProps('createElement').createElement(Markdown, null, c) : c);
-    return ModalActions.openModal(props => {
-        return webpackModules.findByProps('createElement').createElement(ConfirmationModal, Object.assign({
-            header: title,
-            red: danger,
-            confirmText: confirmText,
-            cancelText: cancelText,
-            onConfirm: onConfirm,
-            onCancel: onCancel
-        }, props), content);
-    }, {modalKey: key});
-}
-
-export default {
+export default (data) => {
+  return {
   onLoad() {
     function restartDiscord() {
         if (window.DiscordNative) {
@@ -32,10 +12,9 @@ export default {
             window.location.reload()
         }
     }
-    function CreateModal() {
+    async function CreateModal() {
     try{ 
-        showConfirmationModal("EasyRestart", "Are you sure you want to restart Discord? This will completely terminate Discord and start the updater (reload on web).", {
-            onConfirm: () => restartDiscord()})
+      cumcord.ui.modals.showConfirmationModal({header: "EasyRestart", content: "Are you sure you want to restart Discord? This will completely terminate Discord and start the updater (reload on web).", confirmText: "Restart", type: "danger"}, (confirmed) => {restartDiscord()})
     } catch (e) {
         console.error(e);
     }
@@ -59,4 +38,4 @@ export default {
         }
       };
     document.removeEventListener("keydown", keydownHandler);
-  }}
+  }}}
