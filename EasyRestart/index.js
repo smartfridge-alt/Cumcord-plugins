03e923229ce5547e359a40ab145aa1a5431db9e9
smartfrigde <smartfrigde@users.noreply.github.com>
2022-02-16 17:30:07 +0000

    deploy: 0fe54d520ae9b73e98142baf998fe26e0cfe5604

--- EasyRestart/index.js
+++ EasyRestart/index.js
@@ -1,38 +1,53 @@
 import { showConfirmationModal } from "@cumcord/ui/modals";
 let keydownHandler;
 
-
 export default (data) => {
   return {
-  onLoad() {
-    function restartDiscord() {
+    onLoad() {
+      function restartDiscord() {
         if (window.DiscordNative) {
-            window.DiscordNative.app.relaunch();
+          window.DiscordNative.app.relaunch();
         } else {
-            window.location.reload()
+          window.location.reload();
         }
-    }
-    async function CreateModal() {
-    try{ 
-      showConfirmationModal({header: "EasyRestart", content: "Are you sure you want to restart Discord? This will completely terminate Discord and start the updater (reload on web).", confirmText: "Restart", type: "danger"}, (confirmed) => {restartDiscord()})
-    } catch (e) {
-        console.error(e);
-    }
-    }
-    keydownHandler = async (event) => {
+      }
+      async function CreateModal() {
+        try {
+          showConfirmationModal(
+            {
+              header: "EasyRestart",
+              content:
+                "Are you sure you want to restart Discord? This will completely terminate Discord and start the updater (reload on web).",
+              confirmText: "Restart",
+              type: "danger",
+            },
+            (confirmed) => {
+              if (confirmed) {
+                restartDiscord();
+              }
+            }
+          );
+
+        } catch (e) {
+          console.error(e);
+        }
+      }
+      keydownHandler = async (event) => {
         if (event.code == "F4") {
           event.preventDefault();
           CreateModal();
         }
       };
       document.addEventListener("keydown", keydownHandler);
-  },
-  onUnload() {
-    keydownHandler = async (event) => {
+    },
+    onUnload() {
+      keydownHandler = async (event) => {
         if (event.code == "F4") {
           event.preventDefault();
           CreateModal();
         }
       };
-    document.removeEventListener("keydown", keydownHandler);
-  }}}
+      document.removeEventListener("keydown", keydownHandler);
+    },
+  };
+};
